1865de1f3181624a7aa1687b4b1599a63f1bed7b
Asus <mounesi.post@gmail.com>
2026-02-10 14:05:13 +0330

    initial commit

--- src/Main.java
+++ src/Main.java
@@ -8,36 +8,46 @@ public class Main {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
         Scanner scanner = new Scanner(System.in);
+        float num1,num2;
+        String input1 = "", input2 = "";
+        char operation = ' ';
+            do {
+                try {
+                    System.out.println("Hello and welcome to calculator app!");
+                    System.out.println("You can type exit or press q to exit the program!");
+                    System.out.println("Enter your first number");
+                    input1 = scanner.next();
+                    if (input1.equalsIgnoreCase("exit") || input1.equalsIgnoreCase("q")){
+                        System.out.println("Good-Bye!");
+                        return;
+                    }
+                    System.out.println("Enter operation (+ - * /): ");
 
-            try {
-                float num1,num2;
-                System.out.println("Hello and welcome to calculator app! Enter your first number");
-                String input1 = scanner.next();
-                if (input1.equalsIgnoreCase("exit")){
-                    System.out.println("Good-Bye!");
+                        String opinput =  scanner.next();
+                    if (opinput.equalsIgnoreCase("exit") || opinput.equalsIgnoreCase("q")) {
+                        System.out.println("Good-Bye!");
+                        return;
+                    }else {
+                        operation = opinput.charAt(0);
+                    }
+
+                    System.out.println("Enter your second number   ");
+                    input2 = scanner.next();
+                    if (input2.equalsIgnoreCase("exit")){
+                        System.out.println("Good-Bye!");
+                        return;
+                    }
+
+                    num1= Float.parseFloat(input1);
+                    num2= Float.parseFloat(input2);
+
+                    calc(num1, operation, num2);
+
+                } catch (NumberFormatException nfe) {
+                    System.out.println("dude, you should enter a valid number! ");
                 }
-                System.out.println("Which operation you want to do?  + - / *  ");
-                char operation = scanner.next().charAt(0);
-                System.out.println("Enter your second number   ");
-                String input2 = scanner.next();
-                if (input2.equalsIgnoreCase("exit")){
-                    System.out.println("Good-Bye!");
-                }
-
-                num1= Float.parseFloat(input1);
-                num2= Float.parseFloat(input2);
-
-                if (operation== '/' && num2 == 0){
-                    System.out.println("dude, you cant do division by zero  ");
-                    return;
-                }
-
-                calc(num1, operation, num2);
-
-            } catch (NumberFormatException nfe) {
-                System.out.println("dude, you should enter a valid number! ");
-            }
-
+            }while (!input1.equalsIgnoreCase("exit") || !input2.equalsIgnoreCase("exit") || operation !='q');
+        scanner.close();
         }
     public static void calc(float num1, char operation, float num2) {
 
@@ -52,7 +62,15 @@ public class Main {
                 System.out.println("The result of multiplying your numbers: " + (num1 * num2));
                 break;
             case '/':
-                System.out.println("The result of dividing your numbers: " + (num1 / num2));
+                if(num2 != 0){
+                    System.out.println("The result of dividing your numbers: " + (num1 / num2));
+                    break;
+                }else {
+                    System.out.println("Cant divisible by Zero!");
+                    break;
+                }
+            default:
+                System.out.println("Invalid operation");
                 break;
 
         }
